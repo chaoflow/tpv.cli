192783da378cfdd73dd83aeae889c50046cac83f
Jonas Hoersch <coroa@online.de>
2013-10-15 15:11:25 +0200

    __init__: add comments for completion parts

--- src/tpv/cli/__init__.py
+++ src/tpv/cli/__init__.py
@@ -13,8 +13,11 @@ SwitchAttr = plumbum.cli.SwitchAttr
 autoswitch = plumbum.cli.autoswitch
 switch = plumbum.cli.switch
 
+# decorator to mark arguments supporting Completions
 completion = plumbum.cli.completion
 
+# Completion Objects, which control the completion of arguments
+# refer to plumbum/cli/completion.py
 Completion = plumbum.cli.Completion
 FileCompletion = plumbum.cli.FileCompletion
 DirectoryCompletion = plumbum.cli.DirectoryCompletion
@@ -23,20 +26,43 @@ DynamicCompletion = plumbum.cli.DynamicCompletion
 CallbackDynamicCompletion = plumbum.cli.CallbackDynamicCompletion
 
 class DictDynamicCompletion(DynamicCompletion):
+    '''Completion class to dynamically complete paths in a dicttree
+
+uses / as a separator (obviously).
+
+Usage example:
+
+    PROFILE=dict(foo=dict(foo1=1, foo2=2), bar=3)
+    class profile(tpv.cli.Command):
+        @tpv.cli.completion(name=tpv.cli.DictDynamicCompletion(PROFILE))
+        def __call__(self, name):
+            [...]
+
+then on the command line
+
+    xin profile foo/foTAB
+
+should give foo1 and foo2 as completions.
+    '''
     def __init__(self, dicttree):
         self.dicttree = dicttree
 
     def complete(self, command, prefix):
+        # traverse node into dicttree
         node = self.dicttree
         components = prefix.split("/")
         for comp in components[:-1]:
             node = node[comp]
 
+        # return list of possible matches with the full path
         return ["/".join(components[:-1] + [k]) \
                 + ("/" if isinstance(v, dict) else "")
-                for k,v in node.iteritems() if k.startswith(components[-1])]
+                for k, v in node.iteritems()
+                if k.startswith(components[-1])]
 
     def zsh_action(self, argname):
+        # use a path like completion, then zsh completes
+        # /foo/foTAB to /foo/foo and displays foo1 foo2
         return " __xin_complete_path_like %s" % argname
 
 
